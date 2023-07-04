db12e46e96a27b1756570226008b0810e46e8516
Luděk Roleček <ludek@rolecek.cz>
2023-07-04 19:54:21 +0200

    Aktualizace

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('JS funguje');
+
+
+const nadpisy = document.querySelectorAll('.collapse h2');
+
+nadpisy.forEach( (nadpis) => {
+	nadpis.addEventListener('click', (e) => {
+
+		const otevreny = document.querySelector('.collapse--open');
+		if (otevreny !== null) {
+			otevreny.classList.remove('collapse--open');
+		}
+
+		e.target.parentElement.classList.toggle('collapse--open');
+
+	} );
+} );
+
